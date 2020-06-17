529326df4eef8f176f803cee516f462d6ae64cf3
Alvaro del Castillo <alvarodelcastillo@gmail.com>
2020-06-17 08:29:04 +0200

    WIP: Pipeline create->transform->render

---
+++ mcthings/creator.py
@@ -0,0 +1,21 @@
+# Licensed under the terms of http://www.apache.org/licenses/LICENSE-2.0
+# Author (©): Alvaro del Castillo
+
+# TODO: at some point this must be a real Singleton
+
+
+class Creator:
+    """
+    A creator is the actor which creates the Things in BlocksMemory.
+    It defines all the operations available to create things inside the memory.
+
+    Blocks in memory are organized in width, length, height order
+    """
+
+    @classmethod
+    def set_block(cls, block):
+        pass
+
+    @classmethod
+    def set_blocks(cls, blocks):
+        pass
